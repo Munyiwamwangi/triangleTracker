450f4d594ebb85188ed588853a3bd51de81e4dce
Munyiwamwangi <josseymunyi@gmail.com>
2019-05-30 18:55:32 +0300

    adding logical arguments for equilateral triangle

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 var side1=parseFloat(document.getElementsByClassName('side1'))
 var side2=parseFloat(document.getElementsByClassName('side2'))
 var side3=parseFloat(document.getElementsByClassName('side3'))
-function identifier() {
 
+function identifier() {
+  if(side1 === side2 && side1 === side3 && side2 === side3){
+    console.log("Your values make an equilateral triangle. All sides equal;");
+  }
+
+}
 }
