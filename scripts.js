af0684efb49e0882513da6831f2f40b406ffb78b
Munyiwamwangi <josseymunyi@gmail.com>
2019-05-30 14:15:43 +0300

    introducing function that will identify type of triangles and nest the loops

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 var side1=parseFloat(document.getElementsByClassName('side1'))
 var side2=parseFloat(document.getElementsByClassName('side2'))
 var side3=parseFloat(document.getElementsByClassName('side3'))
+function identifier() {
+
+}
