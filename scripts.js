d1612b5ff3eda511129a4d992faf800c36226b42
Munyiwamwangi <josseymunyi@gmail.com>
2019-05-30 18:58:42 +0300

    argument for equilateral triangle

--- scripts.js
+++ scripts.js
@@ -8,5 +8,12 @@ function identifier() {
   }
 else if(side1 === side2 || side1 === side3 || side2 === side3){
   console.log("Your values give an isosceless triangle. Two sides equal;")
+}
+esle if ((side1+side2<=side3) || (side1+side3<=side2) || (side2_side3<=side1)){
+  console.log("Your can not form any triangle. Sum of two sides should be greater than the remaining side.")
 
+}
+else if (value1!==value2 && value1!==value3 && value2!== value3){
+  console.log("Your values give an scalene triangle. all sides not equal;")
+}
 }
