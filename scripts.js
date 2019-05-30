2106e39c9d0c2ba69a9563cf58b6babfd008c0fb
Munyiwamwangi <josseymunyi@gmail.com>
2019-05-30 14:12:41 +0300

    introducing parseFloat

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 var side1=parseFloat(document.getElementsByClassName('side1'))
 var side2=parseFloat(document.getElementsByClassName('side2'))
-var side3=(document.getElementsByClassName('side3'))
+var side3=parseFloat(document.getElementsByClassName('side3'))
