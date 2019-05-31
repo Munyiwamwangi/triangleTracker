71d580972340f5f5a4c5553e6765821cd1f66a5e
Munyiwamwangi <josseymunyi@gmail.com>
2019-05-31 07:34:20 +0300

    code for not a triangle

--- scripts.js
+++ scripts.js
@@ -2,8 +2,10 @@ var side1=parseFloat(document.getElementsByClassName('side1'))
 var side2=parseFloat(document.getElementsByClassName('side2'))
 var side3=parseFloat(document.getElementsByClassName('side3'))
 
-function identifier() {
-  if(side1 === side2 && side1 === side3 && side2 === side3){
+  if (side1>0 && side2>0 && side3>0){
+
+  }{
+  if(side1===side2 &&side1=== side3 && side2===side3){
     alert("Your values make an equilateral triangle. All sides equal;");
   }
 else if(side1 === side2 || side1 === side3 || side2 === side3){
@@ -12,7 +14,13 @@ else if(side1 === side2 || side1 === side3 || side2 === side3){
 esle if ((side1+side2<=side3) || (side1+side3<=side2) || (side2_side3<=side1)){
   alert("Your can not form any triangle. Sum of two sides should be greater than the remaining side.")
 }
-else (value1!==value2 && value1!==value3 && value2!== value3){
+else if (value1!==value2 && value1!==value3 && value2!== value3){
   alert("Your values give an scalene triangle. all sides not equal;")
+}
+else {
+  alert ("Your sides can not make a triangle; sum of any two sides should be greater than the remaining side")
+}
+else {
+  alert("Values must be greater than zero;")
 }
 }
