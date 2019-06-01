f71053e0e41e6955091b94f3cf12f2501a4b1df7
Munyiwamwangi <josseymunyi@gmail.com>
2019-06-01 17:06:49 +0300

    add READ<E.md

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -3,7 +3,7 @@ function identifier() {
   var side2 = parseFloat(document.getElementById('side2').value);
   var side3 = parseFloat(document.getElementById('side3').value);
 
-  var arrSide = [side1,side2, side3];
+  var arrSide = [side1, side2, side3];
 
   if (side1 > 0 && side2 > 0 && side3 > 0) {
     if (side1 === side2 && side1 === side3 && side2 === side3) {
@@ -16,6 +16,6 @@ function identifier() {
       alert("Your values give an scalene triangle. all sides not equal;");
     }
   } else {
-    alert("Ensure all spaces are filled and, // NOTE: All Values must be greater than zero;");
+    alert("Ensure all spaces are filled and// NOTE:  All Values must be greater than zero;");
   }
 }
